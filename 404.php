f1ec4a4838e3e3d658193bddbb4fb0cd39807d4c
skotperez <voraginebramante@gmail.com>
2014-06-05 15:19:32 +0200

    basic 404 template

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<div id="content" class="container">
+
+<div id="header" class="row">
+	<header class="col-md-16 col-md-offset-3">
+	<h1>Error 404: contenido no encontrado</h1>
+	</header>
+</div><!-- .row -->
+
+<div class="row">
+	<section id="main" class="col-md-16 col-md-push-3">
+		
+	</section>
+</div><!-- .row -->
+
+</div><!-- #content .container -->
+
+<?php get_footer(); ?>
