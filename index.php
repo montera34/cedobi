4fa0e927841989b022e154ba2107942403fdfc7a
skotperez <voraginebramante@gmail.com>
2014-06-16 11:34:41 +0200

    controls buttons conditions for different post types

--- index.php
+++ index.php
@@ -2,73 +2,94 @@
 
 // custom post types info
 global $wp_post_types;
-// get view
-if ( array_key_exists('view', $_GET) ) {
-	$view_current = sanitize_text_field( $_GET['view'] );
-} elseif ( !array_key_exists('view', $_GET) && array_key_exists('s', $_GET) ) {
-	$view_current = "lista";
-} else {
-	$view_current = "mosaico";
-}
 // get post type
 if ( is_home() ) { $pt_current = "archivo"; }
 else { $pt_current = get_post_type(); }
+
 // build filters buttons and page main title
 $base = CEDOBI_BLOGURL;
-$pts = array("brigadista","fotografia","documento","archivo");
-$filters_out = "";
-foreach ( $pts as $pt ) {
-	if ( $pt == $pt_current ) {
-		$active_class = " class='active'";
-		if ( $pt_current == "archivo" ) { $tit = "Archivo digital de las Brigadas Internacionales"; }
-		else { $tit = $wp_post_types[$pt]->labels->name; }
 
-	} else { $active_class = ""; }
-	if ( $pt == 'archivo' ) {
-		$filters_out .= "<div class='col-md-6 col-sm-6 col-xs-12 filter-" .$pt. "'><a" .$active_class. " href='" .$base. "?view=" .$view_current. "'>Archivo completo</a></div>";
+if ( $pt_current == 'archivo' || $pt_current == 'brigadista' || $pt_current == 'fotografia' || $pt_current == 'documento' || is_search() ) {
+// if archivo
+
+	// get view
+	if ( array_key_exists('view', $_GET) ) {
+		$view_current = sanitize_text_field( $_GET['view'] );
+	} elseif ( !array_key_exists('view', $_GET) && array_key_exists('s', $_GET) ) {
+		$view_current = "lista";
 	} else {
-		$pt_tit = $wp_post_types[$pt]->labels->name;
-		$filters_out .= "<div class='col-md-6 col-sm-6 col-xs-12 filter-" .$pt. "'><a" .$active_class. " href='" .$base. "/" .$pt. "?view=" .$view_current. "'>" .$pt_tit. "</a></div>";
+		$view_current = "mosaico";
 	}
 
-}
-// build views buttons
-$views = array("mosaico","lista");
-$views_out = "";
-foreach ( $views as $view ) {
-	if ( $view == $view_current ) { $active_class = " class='active'"; }
-	else { $active_class = ""; }
-	if ( $pt_current == 'archivo' || array_key_exists('s', $_GET) ) {
-		$views_out .= "<div class='col-lg-4 col-md-6 col-sm-5 col-xs-4 vista-" .$view. "'><a" .$active_class. " title='" .$view. "' href='" .$base. "?view=" .$view. "'>" .$view. "</a></div>";
-	} else {
-		$views_out .= "<div class='col-lg-4 col-md-6 col-sm-5 col-xs-4 vista-" .$view. "'><a" .$active_class. " title='" .$view. "' href='" .$base. "/" .$pt_current. "?view=" .$view. "'>" .$view. "</a></div>";
+	// build filters buttons
+	$pts = array("brigadista","fotografia","documento","archivo");
+	$filters_out = "";
+	foreach ( $pts as $pt ) {
+		if ( $pt == $pt_current ) {
+			$active_class = " class='active'";
+			if ( $pt_current == "archivo" ) { $tit = "Archivo digital de las Brigadas Internacionales"; }
+			else { $tit = $wp_post_types[$pt]->labels->name; }
+
+		} else { $active_class = ""; }
+		if ( $pt == 'archivo' ) {
+			$filters_out .= "<div class='col-md-6 col-sm-6 col-xs-12 filter-" .$pt. "'><a" .$active_class. " href='" .$base. "?view=" .$view_current. "'>Archivo completo</a></div>";
+		} else {
+			$pt_tit = $wp_post_types[$pt]->labels->name;
+			$filters_out .= "<div class='col-md-6 col-sm-6 col-xs-12 filter-" .$pt. "'><a" .$active_class. " href='" .$base. "/" .$pt. "?view=" .$view_current. "'>" .$pt_tit. "</a></div>";
+		}
+
 	}
 
-}
+	// build views buttons
+	$views = array("mosaico","lista");
+	$views_out = "";
+	foreach ( $views as $view ) {
+		if ( $view == $view_current ) { $active_class = " class='active'"; }
+		else { $active_class = ""; }
+		if ( $pt_current == 'archivo' || array_key_exists('s', $_GET) ) {
+			$views_out .= "<div class='col-lg-4 col-md-6 col-sm-5 col-xs-4 vista-" .$view. "'><a" .$active_class. " title='" .$view. "' href='" .$base. "?view=" .$view. "'>" .$view. "</a></div>";
+		} else {
+			$views_out .= "<div class='col-lg-4 col-md-6 col-sm-5 col-xs-4 vista-" .$view. "'><a" .$active_class. " title='" .$view. "' href='" .$base. "/" .$pt_current. "?view=" .$view. "'>" .$view. "</a></div>";
+		}
+	}
+
+	// complete controls output
+	$controls_out = "
+	<div id='filters' class='row bair'>
+		<div class='col-lg-12 col-lg-offset-3 col-md-13 col-md-offset-3 col-sm-18'>
+			<div class='filters-tit'>Mostrar</div>
+			<div class='filters-btn row'>
+				" .$filters_out. "
+			</div>
+		</div><!-- .col-* .col-offset-* -->
+		<div class='col-lg-4 col-md-3 col-sm-6'>
+			<div class='filters-tit'>Vista</div>
+			<div class='filters-btn vista-btn row'>
+				" .$views_out. "
+			</div>
+		</div><!-- .col-md-8 -->
+	</div><!-- .row -->
+	";
+	$header_class = "";
+
+} else {
+// if convoca, noticias, publica
+	$tit = $wp_post_types[$pt_current]->labels->name;
+	$view_current = "lista";
+	$controls_out = "";
+	$header_class = " bair";
+} // end if archivo
 ?>
 
 <div id="content" class="container">
 
-<div id="header" class="row">
+<div id="header" class="row<?php echo $header_class ?>">
 	<header class="col-md-16 col-md-offset-3 col-sm-24">
 	<h1><?php echo $tit ?></h1>
 	</header>
 </div><!-- .row -->
 
-<div id="filters" class="row">
-	<div class="col-lg-12 col-lg-offset-3 col-md-13 col-md-offset-3 col-sm-18">
-		<div class="filters-tit">Mostrar</div>
-		<div class="filters-btn row">
-			<?php echo $filters_out ?>
-		</div>
-	</div><!-- .col-* .col-offset-* -->
-	<div class="col-lg-4 col-md-3 col-sm-6">
-		<div class="filters-tit">Vista</div>
-		<div class="filters-btn vista-btn row">
-			<?php echo $views_out ?>
-		</div>
-	</div><!-- .col-md-8 -->
-</div><!-- .row -->
+<?php echo $controls_out; ?>
 
 <div class="row">
 	<section id="main" class="col-md-16 col-md-push-3">
