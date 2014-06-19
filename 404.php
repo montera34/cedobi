0ad5706b15d293b1448a2cd8d96aab4902325c8c
skotperez <voraginebramante@gmail.com>
2014-06-19 09:30:24 +0200

    404 message

--- 404.php
+++ 404.php
@@ -10,8 +10,12 @@
 
 <div class="row">
 	<section id="main" class="col-md-16 col-md-push-3">
-		
+		<p>La página que estás buscando no se ha encontrado, asegúrate de que la has escrito correctamente. Vuelve a la <a href="/">página de inicio</a> o busca lo que quieras sobre las Brigadas Internacionales o el CEDOBI en el buscador.</p>
+		<p>Si deseas cualquier otra información, buscas a algún brigadista o tienes información y documentación de algún brigadista, o sobre las Brigadas Internacionales en general, <a href='/contacto'>ponte en contacto con nosotros</a>.</p>		
 	</section>
+	<div id="margeni" class="col-md-3 col-md-pull-16">
+		<?php get_search_form(); ?>
+	</div><!-- #margeni -->
 </div><!-- .row -->
 
 </div><!-- #content .container -->
