07401652f17d840fe1506ec9233ea64bc9035323
skotperez <voraginebramante@gmail.com>
2015-12-07 17:12:05 +0100

    Fixes bug in dropdown js script

--- js/dropdown.hover.js
+++ js/dropdown.hover.js
@@ -1,10 +1,10 @@
 jQuery(window).ready(function($){
 	$(".menu-item-has-children>a").append(" <span class='glyphicon glyphicon-chevron-down'></span>");
-	$(".menu-item-has-children").click(function(e){
+	$(".menu-item-has-children>a").click(function(e){
 		e.preventDefault();
-		if ( ! $(this).children(".sub-menu").hasClass("sub-active") ) {
+		if ( ! $(this).parent().children(".sub-menu").hasClass("sub-active") ) {
 			$(".sub-active").slideToggle('medium').removeClass("sub-active");
 		}
-		$(this).children(".sub-menu").slideToggle('medium').toggleClass("sub-active");
+		$(this).parent().children(".sub-menu").slideToggle('medium').toggleClass("sub-active");
 	});
 });
