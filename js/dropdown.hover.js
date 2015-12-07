a32d307dd175d4d3c483c06e2b810287729f0469
skotperez <voraginebramante@gmail.com>
2015-12-07 16:46:45 +0100

    fixes bug in dropdown menu click event

--- js/dropdown.hover.js
+++ js/dropdown.hover.js
@@ -1,6 +1,7 @@
 jQuery(window).ready(function($){
 	$(".menu-item-has-children>a").append(" <span class='glyphicon glyphicon-chevron-down'></span>");
-	$(".menu-item-has-children").click(function(){
+	$(".menu-item-has-children").click(function(e){
+		e.preventDefault();
 		if ( ! $(this).children(".sub-menu").hasClass("sub-active") ) {
 			$(".sub-active").slideToggle('medium').removeClass("sub-active");
 		}
