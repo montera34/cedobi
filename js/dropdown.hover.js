5800d2ae5870926f25eb241aa948f6621ccbb07e
skotperez <voraginebramante@gmail.com>
2016-02-09 10:04:17 +0100

    Adds grand child submenu level

--- js/dropdown.hover.js
+++ js/dropdown.hover.js
@@ -2,7 +2,8 @@ jQuery(window).ready(function($){
 	$(".menu-item-has-children>a").append(" <span class='glyphicon glyphicon-chevron-down'></span>");
 	$(".menu-item-has-children>a").click(function(e){
 		e.preventDefault();
-		if ( ! $(this).parent().children(".sub-menu").hasClass("sub-active") ) {
+		if ( ! $(this).parent().children(".sub-menu").hasClass("sub-active") &&
+		     ! $(this).parent().hasClass("sub-btn") ) {
 			$(".sub-active").slideToggle('medium').removeClass("sub-active");
 		}
 		$(this).parent().children(".sub-menu").slideToggle('medium').toggleClass("sub-active");
